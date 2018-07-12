dbfe5b793e961a30f6a84e9627dae71833b923b9
PrivateVoidMethod <kasperjohnsen@live.dk>
2018-07-12 16:40:31 +0200

    Added data from the API

--- app/screens/boxes.js
+++ app/screens/boxes.js
@@ -1,19 +1,55 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { observer } from 'mobx-react/native'
 import boxesStore from '../store/boxesStore'
-import Header  from '../components/Header'
+import Header from '../components/Header'
 
 @observer
 export default class boxes extends Component {
+
+  componentWillMount() {
+    boxesStore.FetchBoxes();
+  }
+
   render() {
+
+    let boxes =  boxesStore.boxes.map((category) => {
+      category.Products.map((product) => {
+        return <Text>{product.Name}</Text>
+      })
+      
+    })
+      
+
     return (
       <View >
-      <Header title={"Kasser"} />
+        <Header title={"Kasser"} />
         <Text>Boxes</Text>
-         {boxesStore.loading &&
-        <Text>LOADING ....</Text>
-          }
+        {boxesStore.loading &&
+          <Text>LOADING ....</Text>
+        }
+
+        {boxesStore.error &&
+          <Text>There was an error</Text>
+        }
+
+      {boxesStore.boxes.map(function (category) {
+            return (
+              <View>
+                <Text style={{color: "red"}}>{category.Name}</Text>
+                {category.Products.map(function (product) {
+                  return (
+                    <View>
+                    <Text>{product.Name}</Text>
+                    <Text>{product.SubTitle}</Text>
+                     <Text>{product.Price}</Text> 
+                     </View>
+                  );
+                })}
+              </View>
+            );
+          })}
+     
       </View>
     );
   }
--- app/store/boxesStore.js
+++ app/store/boxesStore.js
@@ -1,12 +1,38 @@
  import { observable } from 'mobx';
+ import Config from '../../config'
 
 
 class boxesStore {
-    @observable loading = true;
+     @observable boxes = [];
+     @observable error = false;  
+     @observable loading = false;
 
-    StartLoading() {
+    FetchBoxes() {
         this.loading = true;
-        return loading;
-      }    
+        let data = {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          }
+        };
+        return fetch(Config.API_URL + "/products/boxes")
+          .then(response => {
+            if (!response.ok) {
+              return response.json().then(err => {
+                throw err;
+              });
+            }
+            return response.json();
+          })
+          .then(responseJson => {
+            this.boxes = responseJson["d"]["Categories"]
+            this.loading = false;
+          })
+          .catch(error => {
+              this.error = true;
+              this.loading = false;
+          });
+      }     
 };
 export default new boxesStore();
